a375dcecd53f2294d175d8bc1b54d9418c579423
vbali <vibsbali@gmail.com>
2016-11-30 10:40:02 +1100

    Started BinarySearchTree and implemented Add functionality

---
+++ AlgorithmsI.BinarySearchTree/BinaryTree.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+
+namespace AlgorithmsI.BinarySearchTree
+{
+    public class BinaryTree<T>
+        where T : IComparable<T>
+    {
+        private BinaryTreeNode<T> root;
+
+        public void Add(T value)
+        {
+            if (root == null)
+            {
+                root = new BinaryTreeNode<T>(value);
+            }
+            else
+            {
+                AddRecursively(root, value);
+            }
+            Count++;
+        }
+
+        private void AddRecursively(BinaryTreeNode<T> parent, T value)
+        {
+            //If current is 
+            if (parent.CompareTo(value) <= 0)
+            {
+                if (parent.RightNode == null)
+                {
+                    parent.RightNode = new BinaryTreeNode<T>(value);
+                }
+                else
+                {
+                    AddRecursively(parent.RightNode, value);
+                }
+            }
+            else
+            {
+                if (parent.LeftNode == null)
+                {
+                    parent.LeftNode = new BinaryTreeNode<T>(value);
+                }
+                else
+                {
+                    AddRecursively(parent.LeftNode, value);
+                }
+            }
+        }
+
+        public int Count { get; private set; }
+    }
+}
